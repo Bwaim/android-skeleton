08f755d2da1568afdaba4b564f8a4dfc70f58146
Bwaim <fabien.boismoreau@gmail.com>
2021-09-29 00:18:26 +0200

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.0-alpha11")
+    implementation("com.android.tools.build:gradle:7.1.0-alpha12")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     implementation(gradleApi())
     implementation(localGroovy())
