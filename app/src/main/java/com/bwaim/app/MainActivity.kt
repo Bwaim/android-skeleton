679042a832fd6faf7026d04646409934da170495
Bwaim <fabien.boismoreau@gmail.com>
2021-09-29 22:15:59 +0200

    Add navigation support

--- app/src/main/java/com/bwaim/app/MainActivity.kt
+++ app/src/main/java/com/bwaim/app/MainActivity.kt
@@ -3,43 +3,22 @@ package com.bwaim.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.bwaim.compose.theme.SkeletonTheme
+import com.bwaim.navigation.NavigationManager
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 public class MainActivity : ComponentActivity() {
+    @Inject
+    public lateinit var navigationManager: NavigationManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SkeletonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                NavigationGraph(navigationManager = navigationManager)
             }
         }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun DefaultPreview() {
-    SkeletonTheme {
-        Greeting("Android")
     }
 }
